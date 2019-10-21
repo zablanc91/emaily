747680f604ad75e828c2f334f1ed7e3e45cb6768
Carlo Zablan <zablanc91@hotmail.com>
2019-10-20 20:42:35 -0700

    testing subsequent deployment

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express');
 
 const app = express();
 
+//at root route, run the arrow funct, response sent back is an object to display
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({
+        hi: 'there',
+        hello: 'how low?'
+    });
 });
 
 //dynamic port binding for Heroku (will receive at runtime) , otherwise use 5000 for local
