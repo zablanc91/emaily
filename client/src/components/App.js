e482b982a07b0ec82a8788f7481288eae367503c
Carlo Zablan <zablanc91@hotmail.com>
2019-11-20 20:37:53 -0800

    added basic header styled with materialize-css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 //boiler plate testing
-const Header = () => <h2>Header</h2>; 
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>; 
 const SurveyNew = () => <h2>SurveyNew</h2>; 
 const Landing = () => <h2>Landing</h2>; 
