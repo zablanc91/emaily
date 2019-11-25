e2056734137d3999f69132b3b5061d8369834c46
Carlo Zablan <zablanc91@hotmail.com>
2019-11-24 19:29:49 -0800

    implemented Payments component for pay button on dashboard

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent() {
@@ -15,7 +16,12 @@ class Header extends Component {
                 );
             //object if logged in
             default:
-                return <li><a href="api/logout">Logout</a></li>;
+                return [
+                    <li><Payments /></li>,
+                    <li>
+                        <a href="api/logout">Logout</a>
+                    </li>
+                ];
         }
     }
 
